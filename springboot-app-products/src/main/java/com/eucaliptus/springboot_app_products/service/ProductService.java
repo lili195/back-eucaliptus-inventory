46ca785cfd00a393b242432e0de1835dba5d803f
JulianCamiloAyala <juliancamiloayala1407@gmail.com>
2024-11-04 18:27:12 -0500

    GetProductsByProvider trae solo activos

--- springboot-app-products/src/main/java/com/eucaliptus/springboot_app_products/service/ProductService.java
+++ springboot-app-products/src/main/java/com/eucaliptus/springboot_app_products/service/ProductService.java
@@ -26,7 +26,7 @@ public class ProductService {
     }
 
     public List<Product> getProductsByIdProvider(Long idProvider) {
-        return productRepository.findByIdProvider(idProvider);
+        return productRepository.findByIdProviderAndActiveTrue(idProvider);
     }
 
     public Optional<Product> getProductById(String id) {
