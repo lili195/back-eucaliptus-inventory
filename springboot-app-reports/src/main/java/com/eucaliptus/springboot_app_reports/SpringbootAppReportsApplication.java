491d32bc10a83937488a885fe95c8ea06b0ceaad
Leidy Lesmes <leidy.lesmes@uptc.edu.co>
2024-11-19 16:05:47 -0500

    reportes

---
+++ springboot-app-reports/src/main/java/com/eucaliptus/springboot_app_reports/SpringbootAppReportsApplication.java
@@ -0,0 +1,13 @@
+package com.eucaliptus.springboot_app_reports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppReportsApplication.class, args);
+	}
+
+}
