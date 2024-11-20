cdd09ff079b6bf08d0a482fc69cb0952257180d7
JulianCamiloAyala <juliancamiloayala1407@gmail.com>
2024-11-19 23:57:06 -0500

    microservicio notificaciones

---
+++ springboot_app_notifications/src/main/java/com/eucaliptus/springboot_app_notifications/SpringbootAppNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.eucaliptus.springboot_app_notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppNotificationsApplication.class, args);
+	}
+
+}
---
+++ springboot_app_notifications/src/test/java/com/eucaliptus/springboot_app_notifications/SpringbootAppNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eucaliptus.springboot_app_notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAppNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
